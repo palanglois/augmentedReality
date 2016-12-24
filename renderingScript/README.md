7d1fad22a7e503c5d9b90ac1731a7f21047996e0
palanglois <thefroggy27@gmail.com>
2016-12-24 11:16:41 +0100

    Updating the script in order to manage assemblies. Still need to adapt the location of the assembly center of mass.

--- renderingScript/README.md
+++ renderingScript/README.md
@@ -3,5 +3,19 @@
 * Download blender : sudo apt-get install blender
 * Download python 3 : sudo apt-get install python3
 * Set the parameters at the beginning of the file render.py
-* run launch_renderer.sh
+* Create the assembly files (see next section) and put it in the assembly rules folder that you have specified in render.py
+* run launch\_renderer.sh
  
+# The assembly files
+
+The assembly files are csv file that explain blender how to build a renderable assembly from the raw STL files. As a consequence, you must create as much assembly file as classes that you want to render. An example assembly file is given in assembly1.csv
+
+The structure of this file is pretty simple. The first line gives the description of the fields used for each column. Each line after the first one represents one part that is added to the assembly.
+
+Here are the required features for each part : 
+
+* The name of the STL file so that the algorithm can find it. (Just the name is sufficient. The path is known from the parameters that you must have set in render.py)
+* The diffuse color of the part (RGB)
+* Its world matrix which represents its position in space. In order to have it, we recommand to place the assembly in blender. Once the assembly is perfectly fitted, do the following procedure for each part in the assembly : select the part with right click ; open the python3 console in blender and type `bpy.context.active_object.matrix_world`.
+
+
--- renderingScript/render.py
+++ renderingScript/render.py
@@ -1,9 +1,12 @@
 import bpy
 import os, sys
 import numpy
+import csv
 from math import pi
 from bpy import context
-from mathutils import Vector
+from mathutils import Vector, Matrix 
+
+### BEGINNING OF THE USER PARAMETERS ###
 
 #Set the folder where the STL files are located
 localSTLFolder = "/home/thefroggy/Documents/MVA/ObjectRecognition/project/Data/LegoPiecesSTL/"
@@ -13,8 +16,33 @@ localSTLFolder = "/home/thefroggy/Documents/MVA/ObjectRecognition/project/Data/L
 # !!! Must be an empty folder !!! #
 renderingFolder = "/home/thefroggy/Documents/MVA/ObjectRecognition/project/Data/LegoPiecesBlender/"
 
+#Set the assembly rules folder
+
+assemblyFolder = "/home/thefroggy/Documents/MVA/ObjectRecognition/project/Data/assemblyRules/"
+
 #Number of point of views
 nPov = 100
+
+### END OF THE USER PARAMETERS ###
+
+#Parsing the assembly files
+def parseAssemblies(path):
+  assemblies = dict()
+  for file in os.listdir(path): #For each assembly file
+    if file.endswith(".csv"):
+      with open(path+file, 'rt') as f:
+        reader = csv.reader(f,delimiter=';')
+        assembly = []
+        for row in reader: #For each file in the current assembly
+          if row[0] == "PieceName":
+            continue
+          curPiece = dict()
+          curPiece["Name"] = row[0]
+          curPiece["RGB"] = (int(row[1]),int(row[2]),int(row[3]))
+          curPiece["Matrix"] = Matrix(((float(row[4]),float(row[5]),float(row[6]),float(row[7])),(float(row[8]),float(row[9]),float(row[10]),float(row[11])),(float(row[12]),float(row[13]),float(row[14]),float(row[15])),(float(row[16]),float(row[17]),float(row[18]),float(row[19]))))
+          assembly.append(curPiece)
+        assemblies[file[0:-4]] = assembly
+  return assemblies
 
 #Computing the radius
 def computeBoxRad(obj,center):
@@ -25,6 +53,7 @@ def computeBoxRad(obj,center):
     bestRad = curRad if curRad > bestRad else bestRad 
   return bestRad
 
+#Function to clean the blender workspace
 def remove_obj_lamp_and_mesh(context):
     scene = context.scene
     objs = bpy.data.objects
@@ -36,6 +65,7 @@ def remove_obj_lamp_and_mesh(context):
     for mesh in meshes:
         meshes.remove(mesh)
 
+#Get a random point on the surface of the sphere (center,radius)
 def getRandomPointAroundSphere(center,radius):
    theta = numpy.random.uniform(0.,1.)*pi
    phi = numpy.random.uniform(0.,2.)*pi
@@ -44,82 +74,93 @@ def getRandomPointAroundSphere(center,radius):
    z = radius * numpy.cos( theta )
    return (x+center[0],y+center[1],z+center[2])
 
-def getMaterial(red,green,blue):
-  mat = bpy.data.materials.get("Material")
-  if mat is None:
-    # create material
-    mat = bpy.data.materials.new(name="Material")
+#Make a material with the specified name, and the specified color
+def getMaterial(fileName,red,green,blue):
+  mat = bpy.data.materials.new(name="Material"+fileName)
   mat.diffuse_color = [red, green, blue]
   return mat
 
-#Find the files in the folder
-for file in os.listdir(localSTLFolder):
-  if file.endswith(".STL"):
-    os.makedirs(renderingFolder+file[0:-4])
-    remove_obj_lamp_and_mesh(bpy.context)
-    print('Processing file '+file)
+#Creating a lamp with an appropriate energy
+def makeLamp(rad):
+  #Creating a lamp
+  # Create new lamp datablock
+  lamp_data = bpy.data.lamps.new(name="Lamp", type='POINT')
+  lamp_data.distance = rad * 2.5
+  lamp_data.energy = rad/28.6
+  # Create new object with our lamp datablock
+  lamp_object = bpy.data.objects.new(name="Lamp", object_data=lamp_data)
+  # Link lamp object to the scene so it'll appear in this scene
+  scene = bpy.context.scene
+  scene.objects.link(lamp_object)
+  return lamp_object
+
+#Render the current frame with a redirection of the flow in a log file
+def renderWithoutOutput():
+  # redirect output to log file
+  logfile = 'blender_render.log'
+  open(logfile,'a').close()
+  old = os.dup(1)
+  sys.stdout.flush()
+  os.close(1)
+  os.open(logfile, os.O_WRONLY)
+  #Render
+  bpy.ops.render.render(write_still=True) 
+  # disable output redirection
+  os.close(1)
+  os.dup(old)
+  os.close(old)
+
+
+#Rendering procedure for the assemblies
+assemblies = parseAssemblies(assemblyFolder)
+print('Found ' + str(len(assemblies)) + ' assemblies to be rendered')
+for key,assembly in assemblies.items(): #For each assembly
+  remove_obj_lamp_and_mesh(bpy.context) #Cleaning the blender workspace
+  os.makedirs(renderingFolder+key) #Making an appropriate directory
+  print('Processing assembly '+key)
+  for fileInfo in assembly: #For each file in the assembly
+    file = fileInfo["Name"]
     #Import the mesh in current file in blender
     bpy.ops.import_mesh.stl(filepath=localSTLFolder, filter_glob="*.STL",  files=[{"name":localSTLFolder+file}], directory=".")
     #Get the current object
     obj = context.active_object
-    #Get the coordinates of the center of mass
-    local_bbox_center = 0.125 * sum((Vector(b) for b in obj.bound_box), Vector())
-    global_bbox_center = obj.matrix_world * local_bbox_center
-    rad = computeBoxRad(obj,global_bbox_center)
-    #Creating a lamp
-    # Create new lamp datablock
-    lamp_data = bpy.data.lamps.new(name="Lamp", type='POINT')
-    lamp_data.distance = rad * 2.5 
-    lamp_data.energy = rad/28.6
-    # Create new object with our lamp datablock
-    lamp_object = bpy.data.objects.new(name="Lamp", object_data=lamp_data)
-    # Link lamp object to the scene so it'll appear in this scene
-    scene = bpy.context.scene
-    scene.objects.link(lamp_object)
-    rad = 5 * rad
+    #Move object to its place
+    obj.matrix_world = fileInfo["Matrix"]
     # Assign material to object
-    mat = getMaterial(255,0,0)
+    mat = getMaterial(file,*fileInfo["RGB"])
     if obj.data.materials:
     # assign to 1st material slot
       obj.data.materials[0] = mat
     else:
     # no slots
       obj.data.materials.append(mat)
-    for i in range(nPov): #For every point on view
-      xCam,yCam,zCam = getRandomPointAroundSphere(global_bbox_center,rad)
-      if(len(bpy.data.cameras) == 1):
-        objCam = bpy.data.objects['Camera'] # bpy.types.Camera
-        #Place the camera
-        objCam.location.x = xCam
-        objCam.location.y = yCam
-        objCam.location.z = zCam
-        bpy.data.cameras[bpy.context.scene.camera.name].clip_end = 1E6
-        #Point the camera to the object
-        direction = local_bbox_center - Vector([xCam,yCam,zCam])
-        # point the cameras '-Z' and use its 'Y' as up
-        dirUp = 'X' if numpy.random.randint(2) == 0 else 'Y'
-        rot_quat = direction.to_track_quat('-Z', dirUp)
-        # assume we're using euler rotation
-        scene.camera.rotation_mode = 'XYZ'
-        objCam.rotation_euler = rot_quat.to_euler()
-        lamp_object.location = (xCam, yCam, zCam+10)
-        #lamp_object.data = lamp_data
-        #Render the scene 
-        bpy.data.scenes['Scene'].render.filepath = renderingFolder+file[0:-4]+'/'+file[0:-4]+'_'+str(i)+'.jpg'
-
-        # redirect output to log file
-        logfile = 'blender_render.log'
-        open(logfile,'a').close()
-        old = os.dup(1)
-        sys.stdout.flush()
-        os.close(1)
-        os.open(logfile, os.O_WRONLY)
-
-        bpy.ops.render.render(write_still=True) 
-
-        # disable output redirection
-        os.close(1)
-        os.dup(old)
-        os.close(old)
+  obj = context.active_object
+  #Get the coordinates of the center of mass and the radius
+  local_bbox_center = 0.125 * sum((Vector(b) for b in obj.bound_box), Vector())
+  global_bbox_center = obj.matrix_world * local_bbox_center
+  rad = computeBoxRad(obj,global_bbox_center)
+  #Add a lamp (its intensity depends on rad)
+  lamp_object = makeLamp(rad)
+  rad = 5 * rad #We will place the camera away from the object
+  for i in range(nPov): #For every point on view
+    xCam,yCam,zCam = getRandomPointAroundSphere(global_bbox_center,rad)
+    if(len(bpy.data.cameras) == 1):
+      objCam = bpy.data.objects['Camera']
+      #Place the camera
+      objCam.location = (xCam,yCam,zCam)
+      bpy.data.cameras[bpy.context.scene.camera.name].clip_end = 1E6
+      #Point the camera to the object
+      direction = local_bbox_center - Vector([xCam,yCam,zCam])
+      # point the cameras '-Z' and use its 'Y' as up
+      dirUp = 'X' if numpy.random.randint(2) == 0 else 'Y'
+      rot_quat = direction.to_track_quat('-Z', dirUp)
+      # assume we're using euler rotation
+      context.scene.camera.rotation_mode = 'XYZ'
+      objCam.rotation_euler = rot_quat.to_euler()
+      #Place the lamp
+      lamp_object.location = (xCam, yCam, zCam+10)
+      #Render the scene 
+      bpy.data.scenes['Scene'].render.filepath = renderingFolder+key+'/'+key+'_'+str(i)+'.png'
+      renderWithoutOutput()
 
 
